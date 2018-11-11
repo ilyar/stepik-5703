0b418a4cc09af1ca76eaf3f690922d94df090529
Ilya Rogov <ilyar.software@gmail.com>
2018-11-12 00:06:54 +0200

    4. Display data in lists via ScrollView

--- app/src/main/java/org/stepik/a5703/myapplication/MainActivity.kt
+++ app/src/main/java/org/stepik/a5703/myapplication/MainActivity.kt
@@ -1,12 +1,13 @@
 package org.stepik.a5703.myapplication
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.gson.Gson
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 const val URL_FEED: String =
@@ -14,43 +15,36 @@ const val URL_FEED: String =
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var vText: TextView
+    lateinit var vList: LinearLayout
+    var request: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("live", "MainActivity.onCreate")
         setContentView(R.layout.activity_main)
 
-        vText = findViewById<TextView>(R.id.activity_main__text)
-        vText.setTextColor(0xFFFFF0000.toInt())
-        vText.setOnClickListener {
-            Log.d("live", "MainActivity.vText.setOnClickListener")
+        vList = findViewById<LinearLayout>(R.id.activity_main__list)
+        val o = createRequest(URL_FEED)
+            .map { Gson().fromJson(it, Feed::class.java) }
+            .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
-            val o = createRequest(URL_FEED)
-                .map { Gson().fromJson(it, Feed::class.java) }
-                .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
-
-            o.subscribe({
-                // result
-                Log.d("live", "MainActivity.request.result")
-                for (item in it.items) {
-                    Log.d("live", "title: ${item.title}")
-                }
-            }, {
-                // error
-                Log.d("live", "MainActivity.request.error:  ${it.message}")
-            })
-        }
+        request = o.subscribe({
+            // result
+            Log.d("live", "MainActivity.request.result")
+            showLinearLayout(it.items)
+        }, {
+            // error
+            Log.d("live", "MainActivity.request.error:  ${it.message}")
+        })
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Log.d("live", "MainActivity.onActivityResult $requestCode $resultCode")
-
-        if (data !== null) {
-            val str = data.getStringExtra("textFromInner")
-
-            vText.text = str
+    fun showLinearLayout(feedList: ArrayList<FeedItem>) {
+        val inflater = layoutInflater
+        for (item in feedList) {
+            val view = inflater.inflate(R.layout.activity_main__item, vList, false)
+            val vTitle = view.findViewById<TextView>(R.id.activity_main__title)
+            vTitle.text = item.title
+            vList.addView(view)
         }
     }
 
